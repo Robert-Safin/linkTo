d34b6dfde7744d42057a92540e0f715684ba90e3
Robert Safin <robertsafin98@gmail.com>
2023-08-22 16:34:57 +0800

    re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,28 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-  const user = await currentUser()
-  if(user) {
-    redirect(`${user.id}`)
+  const user = await currentUser();
+  if (user) {
+    redirect(`${user.id}`);
   }
   return (
     <div className="flex justify-center items-center h-screen bg-lightestGray">
       <div className="rounded-lg p-4 m-4">
         <h1 className="headerM">linkTo is a free link sharing app.</h1>
-        <p className="bodyM my-4">Generate your personal page with links to your social platforms.</p>
+        <p className="bodyM my-4">
+          Generate your personal page with links to your social platforms.
+        </p>
         <div className="flex flex-col w-full justify-evenly space-y-4">
-          <Link href={`/signIn`}className="buttonPrimaryDefault text-center">
+          <Link
+            href={`https://clear-rabbit-33.accounts.dev/sign-in`}
+            className="buttonPrimaryDefault text-center"
+          >
             Sign In
           </Link>
-          <Link href={`/signUp`}className="buttonPrimaryDefault text-center">
+          <Link
+            href={`https://clear-rabbit-33.accounts.dev/sign-up`}
+            className="buttonPrimaryDefault text-center"
+          >
             Sign Up
           </Link>
         </div>
