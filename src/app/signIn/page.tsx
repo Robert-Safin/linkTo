fc0f638a9cc8d39da1bf996507cfd091e4785195
Robert Safin <robertsafin98@gmail.com>
2023-08-22 16:32:38 +0800

    re

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 const SignInPage = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignIn afterSignInUrl={'/home'}/>
+      <SignIn/>
     </div>
   )
 }
--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 const signUpPage = async () => {
   return (
     <div className="flex justify-center items-center h-screen">
-    <SignUp afterSignUpUrl={'/home'}/>
+    <SignUp/>
   </div>
   )
 };
