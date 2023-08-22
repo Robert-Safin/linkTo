11b0ed585cc252645263aaba8394d42a4deecd38
Robert Safin <robertsafin98@gmail.com>
2023-08-22 15:19:45 +0800

    re

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 const signUpPage = async () => {
   return (
     <div className="flex justify-center items-center h-screen">
-    <SignUp/>
+    <SignUp afterSignUpUrl={'/home'}/>
   </div>
   )
 };
